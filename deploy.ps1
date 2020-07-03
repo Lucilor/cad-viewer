40a3efa0b9b8db41f82e1238548f62ead97de442
Lucilor <lucilor@me.com>
2020-07-03 16:26:03 +0800

    update: package.json

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
-copy-item "F:\Lucilor\ng-cad\src\app\cad-viewer\*" ".\src" -Force -Recurse
+copy-item "F:\Lucilor\ng-cad2\src\app\cad-viewer\*" ".\src" -Force -Recurse
 yarn lib && yarn dist
 copy-item ".\dist\*" "D:\wwwroot\www.n.com\n\static\js\jichu" -Force -Recurse
--- git.ps1
+++ git.ps1
@@ -1,5 +1,13 @@
 $commitMsg = $args[0]
+if ($commitMsg -eq $null) {
+    Write-Host 'error: missing commit message'
+    exit
+}
 git add .
-git commit -m"'$commitMsg'"
-git pull
-git push
+git status
+$decision = $Host.UI.PromptForChoice('', 'Continue?', @('&Yes', '&No'), 0)
+if ($decision -eq 0) {
+    git commit -m"$commitMsg"
+    git pull
+    git push
+}
