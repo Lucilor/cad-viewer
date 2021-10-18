16942c4059877d720cc63e67e3681c0218cd9f57
Lucilor <lucilor@me.com>
2021-10-18 11:27:29 +0800

    ✨ feat(cad-data): cad version

--- src/cad-data/cad-data.ts
+++ src/cad-data/cad-data.ts
@@ -12,6 +12,21 @@ export interface CadDataInfo {
     唯一码?: string;
     修改包边正面宽规则?: string;
     锁边自动绑定可搭配铰边?: string;
+    version?: CadVersion;
+}
+
+export enum CadVersion {
+    DXF9 = "AC1004",
+    DXF10 = "AC1006",
+    DXF12 = "AC1009",
+    DXF13 = "AC1012",
+    DXF14 = "AC1014",
+    DXF2000 = "AC1015",
+    DXF2004 = "AC1018",
+    DXF2007 = "AC1021",
+    DXF2010 = "AC1024",
+    DXF2013 = "AC1027",
+    DXF2018 = "AC1032"
 }
 
 export class CadData {
